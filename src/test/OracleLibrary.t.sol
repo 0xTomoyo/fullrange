b87d16258e5eb4941a5c01aeaa9e7ba5cde45564
0xTomoyo <0xtomoyo@protonmail.com>
2021-12-31 20:17:08 +1100

    OracleLibrary tests

---
+++ src/test/OracleLibrary.t.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+import {DSTest} from "ds-test/test.sol";
+import {OracleLibrary} from "../libraries/OracleLibrary.sol";
+
+contract MockObservable {
+    Observation private observation0;
+    Observation private observation1;
+
+    struct Observation {
+        uint32 secondsAgo;
+        int56 tickCumulatives;
+        uint160 secondsPerLiquidityCumulativeX128s;
+    }
+
+    constructor(
+        uint32[] memory secondsAgos,
+        int56[] memory tickCumulatives,
+        uint160[] memory secondsPerLiquidityCumulativeX128s
+    ) {
+        require(
+            secondsAgos.length == 2 && tickCumulatives.length == 2 && secondsPerLiquidityCumulativeX128s.length == 2,
+            "Invalid test case size"
+        );
+
+        observation0 = Observation(secondsAgos[0], tickCumulatives[0], secondsPerLiquidityCumulativeX128s[0]);
+        observation1 = Observation(secondsAgos[1], tickCumulatives[1], secondsPerLiquidityCumulativeX128s[1]);
+    }
+
+    function observe(uint32[] calldata secondsAgos)
+        external
+        view
+        returns (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s)
+    {
+        require(
+            secondsAgos[0] == observation0.secondsAgo && secondsAgos[1] == observation1.secondsAgo,
+            "Invalid test case"
+        );
+
+        int56[] memory _tickCumulatives = new int56[](2);
+        _tickCumulatives[0] = observation0.tickCumulatives;
+        _tickCumulatives[1] = observation1.tickCumulatives;
+
+        uint160[] memory _secondsPerLiquidityCumulativeX128s = new uint160[](2);
+        _secondsPerLiquidityCumulativeX128s[0] = observation0.secondsPerLiquidityCumulativeX128s;
+        _secondsPerLiquidityCumulativeX128s[1] = observation1.secondsPerLiquidityCumulativeX128s;
+
+        return (_tickCumulatives, _secondsPerLiquidityCumulativeX128s);
+    }
+}
+
+contract OracleLibraryTest is DSTest {
+    function testConsult() public {
+        uint32 period = 3;
+        uint32[] memory secondsAgos = new uint32[](2);
+        secondsAgos[0] = period;
+        secondsAgos[1] = 0;
+        int56[] memory tickCumulatives = new int56[](2);
+        tickCumulatives[0] = 12;
+        tickCumulatives[1] = 12;
+        uint160[] memory secondsPerLiquidityCumulativeX128s = new uint160[](2);
+        secondsPerLiquidityCumulativeX128s[0] = 10;
+        secondsPerLiquidityCumulativeX128s[1] = 20;
+
+        MockObservable observable = new MockObservable(
+            secondsAgos,
+            tickCumulatives,
+            secondsPerLiquidityCumulativeX128s
+        );
+
+        assertEq(OracleLibrary.consult(address(observable), period, 0, 0, 0), 0);
+    }
+}
